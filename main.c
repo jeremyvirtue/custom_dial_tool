8e19db30a84e9b1f3864b4a088d22b7a0cfae73e
EA_Jeremy <1037063188@qq.com>
2021-08-09 19:57:32 +0800

    移植json

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  * @Author: Jeremy
  * @Date: 2021-08-09 14:23:55
  * @LastEditors: Jeremy
- * @LastEditTime: 2021-08-09 18:25:54
+ * @LastEditTime: 2021-08-09 19:39:40
  */
 /*
  * @LastEditTime: 2021-08-09 13:40:08
@@ -13,6 +13,7 @@
 #include <string.h>
 #include <stdint.h>
 #include "custom_dial_head.h"
+#include "cJSON/cJSON.h"
 
 uint8_t dial_data[4096];
 uint8_t obj[40]; 
@@ -302,9 +303,84 @@ uint8_t get_dial_type_size(uint8_t type)
 }
 
 
- 
+ char *message = 
+"{                              \
+    \"name\":\"mculover666\",   \
+    \"age\": 22,                \
+    \"weight\": 55.5,           \
+    \"address\":                \
+        {                       \
+            \"country\": \"China\",\
+            \"zip-code\": 111111\
+        },                      \
+    \"skill\": [\"c\", \"Java\", \"Python\"],\
+    \"student\": false          \
+}";
+
+
+
 int main()
 {
+  printf("Version: %s\n", cJSON_Version());
+  cJSON* cjson_test = NULL;
+  cJSON* cjson_name = NULL;
+  cJSON* cjson_age = NULL;
+  cJSON* cjson_weight = NULL;
+  cJSON* cjson_address = NULL;
+  cJSON* cjson_address_country = NULL;
+  cJSON* cjson_address_zipcode = NULL;
+  cJSON* cjson_skill = NULL;
+  cJSON* cjson_student = NULL;
+  int    skill_array_size = 0, i = 0;
+  cJSON* cjson_skill_item = NULL;
+
+  /* 解析整段JSO数据 */
+  cjson_test = cJSON_Parse(message);
+  if(cjson_test == NULL)
+  {
+      printf("parse fail.\n");
+      return -1;
+  }
+
+  /* 依次根据名称提取JSON数据（键值对） */
+  cjson_name = cJSON_GetObjectItem(cjson_test, "name");
+  cjson_age = cJSON_GetObjectItem(cjson_test, "age");
+  cjson_weight = cJSON_GetObjectItem(cjson_test, "weight");
+
+  printf("name: %s\n", cjson_name->valuestring);
+  printf("age:%d\n", cjson_age->valueint);
+  printf("weight:%.1f\n", cjson_weight->valuedouble);
+
+  /* 解析嵌套json数据 */
+  cjson_address = cJSON_GetObjectItem(cjson_test, "address");
+  cjson_address_country = cJSON_GetObjectItem(cjson_address, "country");
+  cjson_address_zipcode = cJSON_GetObjectItem(cjson_address, "zip-code");
+  printf("address-country:%s\naddress-zipcode:%d\n", cjson_address_country->valuestring, cjson_address_zipcode->valueint);
+
+  /* 解析数组 */
+  cjson_skill = cJSON_GetObjectItem(cjson_test, "skill");
+  skill_array_size = cJSON_GetArraySize(cjson_skill);
+  printf("skill:[");
+  for(i = 0; i < skill_array_size; i++)
+  {
+      cjson_skill_item = cJSON_GetArrayItem(cjson_skill, i);
+      printf("%s,", cjson_skill_item->valuestring);
+  }
+  printf("\b]\n");
+
+  /* 解析布尔型数据 */
+  cjson_student = cJSON_GetObjectItem(cjson_test, "student");
+  if(cjson_student->valueint == 0)
+  {
+      printf("student: false\n");
+  }
+  else
+  {
+      printf("student:error\n");
+  }
+    
+
+
 
   uint8_t crc8[8] = {0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff};
   uint8_t head_bin4[4] = { 0x00,0x00,0xa0,0x06};
