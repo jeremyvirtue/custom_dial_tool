95b7246b094bba5a3c12d93c5aeb0eefbe02f02a
EA_Jeremy <1037063188@qq.com>
2021-08-13 17:49:39 +0800

    修改协议

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  * @Author: Jeremy
  * @Date: 2021-08-09 14:23:55
  * @LastEditors: Jeremy
- * @LastEditTime: 2021-08-10 18:09:52
+ * @LastEditTime: 2021-08-13 17:46:45
  */
 /*
  * @LastEditTime: 2021-08-09 13:40:08
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "custom_dial_head.h"
 #include "cJSON/cJSON.h"
 
@@ -196,14 +197,63 @@ void main9_data_init(void)
 	memory += get_dial_type_size(dial_type_text);
 }
 
+
+ 
+/* 返回ch字符在sign数组中的序号 */
+int getIndexOfSigns(char ch)
+{
+    if(ch >= '0' && ch <= '9')
+    {
+        return ch - '0';
+    }
+    if(ch >= 'A' && ch <='F') 
+    {
+        return ch - 'A' + 10;
+    }
+    if(ch >= 'a' && ch <= 'f')
+    {
+        return ch - 'a' + 10;
+    }
+    return -1;
+}
+
+long hexToDec(char *source)
+{
+    long sum = 0;
+    long t = 1;
+    int i, len;
+ 
+    len = strlen(source);
+    for(i=len-1; i>=0; i--)
+    {
+        sum += t * getIndexOfSigns(*(source + i));
+        t *= 16;
+    }  
+    printf("sum = %ld\n",sum);
+    return sum;
+}
+ 
+  /*
+  */
+uint32_t get16CharToInt(char* s)
+{
+  uint32_t num;
+  s+=2;
+  num = hexToDec(s);
+  // printf("%s\n",s);
+  // num = aoti(s);
+
+  return num;
+}
+  
+
 int main()
 {
   printf("Version: %s\n", cJSON_Version()); 
   uint8_t obj_num = 0;
-	uint8_t* memory = dial_data; 
+	uint8_t* memory = dial_data;  
 
-
-
+  
   int    skill_array_size = 0, i = 0,j; 
   char read_json[65535] = {0};
   char *ch;
@@ -231,8 +281,17 @@ int main()
       printf("parse fail.\n"); 
       return 0;
   }
+  cJSON* cjson_dial_id = cJSON_GetObjectItem(cjson_mytest, "dial_id");
+  if(cjson_dial_id == NULL)
+  {
+    printf("get id fail\n");
+    return 0;
+  }
+
+
+
   printf("cJSON_GetArraySize = %d\n",cJSON_GetArraySize(cjson_mytest));
-  for(j = 0;j<cJSON_GetArraySize(cjson_mytest);j++)
+  for(j = 1;j<cJSON_GetArraySize(cjson_mytest);j++)
   { 
     cJSON* obj = cJSON_GetArrayItem(cjson_mytest,j); 
     printf("obj[%d] = %s\n",j,obj->string); 
@@ -247,7 +306,7 @@ int main()
       dat.type     = json_type->valueint;
       dat.x        = json_x->valueint;
       dat.y        = json_y->valueint;
-      dat.img_addr = json_img_addr->valueint;
+      dat.img_addr = get16CharToInt(json_img_addr->valuestring);
       printf("%d %d %d %d \n",dat.type,dat.x,dat.y,dat.img_addr);
       obj_type[obj_num++] = dat.type;
       memcpy(memory,&dat,get_dial_type_size(dial_type_image)); 
@@ -271,7 +330,7 @@ int main()
       dat.data_type         =  json_data_type->valueint;
       dat.x                 =  json_x->valueint;
       dat.y                 =  json_y->valueint;
-      dat.img_addr          =  json_img_addr->valueint;
+      dat.img_addr          =  get16CharToInt(json_img_addr->valuestring);
       dat.img_heigth        =  json_img_heigth->valueint;
       dat.img_width         =  json_img_width->valueint;
       dat.start_arc         =  json_start_arc->valueint;
@@ -297,11 +356,11 @@ int main()
       cJSON* cjson_text_custom    =  cJSON_GetObjectItem(obj,"text_custom"); if(cjson_text_custom == NULL) {printf("format err\n");return 0;}                    
 
       custom_dial_text dat;
-      dat.type            =              cjson_type->valueint;                   
-      dat.data_type       =   cjson_data_type->valueint;                          
-      dat.x               =   cjson_x->valueint;                                  
-      dat.y               =    cjson_y->valueint;                                
-      dat.text_width      =    cjson_text_width->valueint;                       
+      dat.type            = cjson_type->valueint;                   
+      dat.data_type       = cjson_data_type->valueint;                          
+      dat.x               = cjson_x->valueint;                                  
+      dat.y               = cjson_y->valueint;                                
+      dat.text_width      = cjson_text_width->valueint;                       
       if(cJSON_GetArraySize(cjson_text_rgb) == 3){
         cJSON* r =cJSON_GetArrayItem(cjson_text_rgb,0);
         cJSON* g =cJSON_GetArrayItem(cjson_text_rgb,1);
@@ -343,7 +402,7 @@ int main()
       for(i = 0;i<cJSON_GetArraySize(cjson_img_addr);i++)
       {
         cJSON* r =cJSON_GetArrayItem(cjson_img_addr,i);
-        dat.img_addr[i] = r->valueint;
+        dat.img_addr[i] = get16CharToInt(r->valuestring);
       }
       printf("%d %d %d %d ",dat.type,dat.data_type ,dat.x,dat.y);
       for(i = 0;i<cJSON_GetArraySize(cjson_img_addr);i++)
@@ -383,17 +442,47 @@ int main()
 
   // fp = fopen("obj_data.bin", "w+");
   // fwrite(&dial_data,sizeof(dial_data),1,fp);  
-  // fclose(fp);
+  // fclose(fp); 
   
-  fp = fopen("001001_U0.3.bin", "w+");
+	// FILE *f_rb;
+  // int c;
+  // f_rb = fopen("AllBinData.bin","rb");
+
+  char file_name[] = "001001_U0.";
+  strcat(file_name,cjson_dial_id->valuestring);
+  strcat(file_name,".bin");
+
+  fp = fopen(file_name, "w+");
   fwrite(&head_bin4,sizeof(head_bin4),1,fp);  
   fwrite(&crc8,sizeof(crc8),1,fp);  
   fwrite(&obj_type,sizeof(obj_type),1,fp);  
   fwrite(&dial_data,sizeof(dial_data),1,fp);  
-  fclose(fp);
 
+  // // while((	c=fgetc(f_rb))!=EOF) { putc(c, fp);}	
+  fwrite(&crc8,sizeof(crc8),1,fp);   
+  // fclose(f_rb); 
+  fclose(fp);
+  printf("ok\n");
   
 
+  
+	// FILE *f_rb,*f_wb;
+	// long n,offset;
+	// int c;
+	// f_rb = fopen("AllBinData.bin","rb");
+	
+	// f_wb = fopen("001001_R0.5.bin","w+");
+	
+	// // fseek(f_wb,0,SEEK_END);
+	// fputc('\x00',f_wb);//0X00
+	// fputc('\x00',f_wb);//0X00
+	// fputc('\x02',f_wb);//0X00
+	// fputc('\x04',f_wb);//0X00
+	// while((	c=fgetc(f_rb))!=EOF) fputc(c, f_wb);	
+	// fclose(f_rb);
+
+
+	// fclose(f_wb);
 
 
   
---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+###
+ # @Descripttion: 
+ # @version: 
+ # @Author: Jeremy
+ # @Date: 2021-08-11 14:24:23
+ # @LastEditors: Jeremy
+ # @LastEditTime: 2021-08-11 17:05:18
+### 
+
+./main
+
+cp 001001_R0.5.bin /mnt/share/
